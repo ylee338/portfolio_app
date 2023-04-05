cec8215160c4ffb10330dd6a642682314b745f6f
Young <ylee1359256@gmail.com>
2023-04-05 01:55:00 -0400

    created columns, images, and content

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+column_1, column_2 = st.columns(2)
+
+with column_1:
+    st.image("images/photos.png")
+
+with column_2:
+    st.title("Young Lee")
+    content = """I'm currently a GMU student graduating Fall 2023. I major in Information Technology with a 
+    concentration in  cyber security. I've built this web app to showcase myself and my knowledge in programming. 
+    I have a great desire to learn new concepts and technologies to further my career and knowledge. On my free 
+    time I enjoy reading comics, discovering new music, and spending time with friends. Some of my hobbies are 
+    working out, playing basketball, and programming.  
+    """
+    st.info(content)
