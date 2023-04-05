54a283ea042f2c24bb27c19dc966ae6b338d52f2
Young <ylee1359256@gmail.com>
2023-04-05 02:35:31 -0400

    added description, image, and links

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
-column_1, column_2 = st.columns(2)
+column_1, empty_column, column_2 = st.columns([1.5, 0.5, 1.5])
 
 with column_1:
     st.image("images/photos.png", width= 500)
@@ -17,3 +18,21 @@ with column_2:
     """
     st.info(content)
 st.write("**Below you can find some of my personal projects I've created. Feel free to contact me!**")
+
+column_3, column_4 = st.columns(2)
+
+data_file = pandas.read_csv('data.csv', sep=";")
+
+with column_3:
+    for index, row in data_file[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
+with column_4:
+    for index, row in data_file[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
